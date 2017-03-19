dda36f064b984fd593e8499bd585216ecb4dca44
root <root@epbyminw2033t2.minsk.epam.com>
2017-03-19 19:34:15 +0300

    hometask2

---
+++ hometask2/README.md
@@ -0,0 +1,9 @@
+# Hometasks for Python Devops 2017
+
+Contains tasks:
+task1.py  
+task2.py  
+task3.py  
+task4.py  
+zadanie_pro_spisok.py
+
--- zadanie_pro_spisok.py
+++
@@ -1,9 +0,0 @@
-# Vyvesti chislo unikal'nykh elementov spiska
-
-a = [10, 0, 1, 2, 1, 10, 2, 3, 5, 6]
-
-spis = []  # dlya isklyucheniya dubliruyuschihsya zapisei pro element
-for _ in range(len(a)):
-    if a[_] not in spis:
-        print("Element {} vstrechaetsya {} raz".format(a[_], a.count(a[_])))
-        spis.append(a[_])
